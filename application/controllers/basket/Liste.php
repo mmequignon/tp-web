7f2fa60cc5d9a81bb3d535b3560b28ed2684c067
Matthieu MÉQUIGNON <matthieu@fwzte.xyz>
2017-07-03 23:40:45 +0200

    First Commit

--- application/controllers/basket/Liste.php
+++ application/controllers/basket/Liste.php
@@ -4,13 +4,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Liste extends CI_Controller {
 
 	public function index(){
+        // On récupère les informations de la session courante
+        // afin de les passer aux vues
+        $this->load->library('session');
         $this->load->model('Product');
         $this->load->model('Basket');
         $this->load->model('BasketLine');
         $this->load->model('PaymentMode');
-        $this->load->library('session');
-        // On récupère les informations de la session courante
-        // afin de les passer aux vues
         $user_id = $this->session->userdata('id');
         $admin = $this->session->userdata('admin'); 
         $logged = $this->session->userdata('logged'); 
@@ -20,18 +20,22 @@ class Liste extends CI_Controller {
         $delivery_modes = $this->Product->get_delivery_modes();
         $payment_modes = $this->PaymentMode->get_all();
         $basket = $this->Basket->get_basket($basket_id);
+        $amount = $basket->amount;
+        $title = ($basket->amount == 0) ? "Empty" : "Total : ".$basket->amount."€";
+
         $data = array(
             "page" => "basket",
-            "title" => "Basket - Total : ".$basket->amount."€",
+            "title" => "Basket - ".$title,
             "logged" => $logged,
             "basket_lines" => $basket_lines,
             "delivery_modes" => $delivery_modes,
             "payment_modes" => $payment_modes,
-            "amount" => $basket->amount,
+            "amount" => $amount,
             "admin" => $admin,
             "basket_id" => $basket_id,
             "user_id" => $user_id
         );
+
 		$this->load->view('base/header', $data);
 		$this->load->view('basket/liste', $data);
 		$this->load->view('base/footer');
